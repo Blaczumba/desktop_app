7742c0ba0a6d69830f2af8fb76fac7219d97680d
Blaczumba <93483385+Blaczumba@users.noreply.github.com>
2026-03-04 17:39:02 +0100

    Layered approach for presentation and graphics API
    * Integrated with graphics context approach
    
    * Integrated with BejzakEngine
    
    ---------
    
    Co-authored-by: Bejzak <ejzakblazej@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
-#include "application.h"
-
 #include <memory>
 #include <print>
 
 #include "bejzak_engine/common/util/engine_exception.h"
 #include "bejzak_engine/vulkan/wrapper/util/check.h"
 #include "bejzak_engine/common/file/standard_file_loader.h"
+#include "bejzak_engine/vulkan/graphics_context/presentation.h"
+#include "bejzak_engine/common/window/window_glfw.h"
 
 int main() {
   try {
-	Application app(std::make_unique<StandardFileLoader>());
-	app.run();
+	auto fileLoader = std::make_unique<StandardFileLoader>();
+	auto application = vlkn::Presentation::create(std::make_unique<WindowGlfw>("BejzakEngine", 1920, 1080), *fileLoader);
+	application->run();
   }
   catch (const VkException& vkException) {
-	  std::println("Vulkan exception occured with message: {} and VkResult code: {}. \n {}", vkException.what(), static_cast<std::int32_t>(vkException.getResult()), vkException.stackTrace());
+	  std::println("Vulkan exception occured with message: {} and VkResult code: {}", vkException.what(), static_cast<std::int32_t>(vkException.getResult()));
   }
   catch (const EngineException& engineException) {
-	  std::println("Vulkan exception occured with message: {}. \n {}", engineException.what(), engineException.stackTrace());
+	  std::println("Vulkan exception occured with message: {}. \n", engineException.what());
   }
 
   return EXIT_SUCCESS;
