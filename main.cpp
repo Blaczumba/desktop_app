d570ba150f7c456ea3ba50ac084ed08ccbc2f420
Bejzak <ejzakblazej@gmail.com>
2026-09-22 09:14:56 +0200

    Compliant with new way of handling managers

--- main.cpp
+++ main.cpp
@@ -8,17 +8,17 @@
 #include "bejzak_engine/common/window/window_glfw.h"
 
 int main() {
-  try {
-	auto fileLoader = std::make_unique<StandardFileLoader>();
-	auto application = vlkn::Presentation::create(std::make_unique<WindowGlfw>("BejzakEngine", 1920, 1080), *fileLoader);
-	application->run();
-  }
-  catch (const VkException& vkException) {
-	  std::println("Vulkan exception occured with message: {} and VkResult code: {}", vkException.what(), static_cast<std::int32_t>(vkException.getResult()));
-  }
-  catch (const EngineException& engineException) {
-	  std::println("Vulkan exception occured with message: {}. \n", engineException.what());
-  }
+	try {
+		auto fileLoader = std::make_unique<StandardFileLoader>();
+		auto application = vlkn::Presentation::create(std::make_unique<WindowGlfw>("BejzakEngine", 1920, 1080, false), *fileLoader);
+		application->run();
+	}
+	catch (const VkException& vkException) {
+		std::println("Vulkan exception occured with message: {} and VkResult code: {}", vkException.what(), static_cast<std::int32_t>(vkException.getResult()));
+	}
+	catch (const EngineException& engineException) {
+		std::println("Vulkan exception occured with message: {}. \n", engineException.what());
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
